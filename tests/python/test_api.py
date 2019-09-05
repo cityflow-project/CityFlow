358be8979d10d2b63046c2e1db108c7af9d2f13e
zhc134 <me@zhanghuichu.com>
2019-09-05 13:22:20 +0800

    pep8 style

--- tests/python/test_api.py
+++ tests/python/test_api.py
@@ -10,7 +10,7 @@ class TestAPI(unittest.TestCase):
     def test_data_api(self):
         """Single save and single load with single threading engine"""
         eng = cityflow.Engine(config_file=self.config_file, thread_num=1)
-        
+
         for _ in range(self.period):
             eng.next_step()
             running_count = len(eng.get_vehicles())
@@ -28,11 +28,11 @@ class TestAPI(unittest.TestCase):
 
     def test_set_replay(self):
         """change replay path on the fly"""
-        eng = cityflow.Engine(config_file = self.config_file, thread_num = 1)
+        eng = cityflow.Engine(config_file=self.config_file, thread_num=1)
 
         for _ in range(100):
             eng.next_step()
-        
+
         eng.set_replay_file("replay2.txt")
 
         for _ in range(100):
