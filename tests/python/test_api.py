c3db8fd40a8cbfc60bd6315d1ba26d6f200b0ed6
zhc134 <44251346+zhc134@users.noreply.github.com>
2019-09-05 13:43:55 +0800

    feat(engine): set_replay_file during runtime (#31)
    * feat(engine): set_replay_file during runtime
    
    * pep8 style

--- tests/python/test_api.py
+++ tests/python/test_api.py
@@ -10,7 +10,7 @@ class TestAPI(unittest.TestCase):
     def test_data_api(self):
         """Single save and single load with single threading engine"""
         eng = cityflow.Engine(config_file=self.config_file, thread_num=1)
-        
+
         for _ in range(self.period):
             eng.next_step()
             running_count = len(eng.get_vehicles())
@@ -26,6 +26,19 @@ class TestAPI(unittest.TestCase):
 
         del eng
 
+    def test_set_replay(self):
+        """change replay path on the fly"""
+        eng = cityflow.Engine(config_file=self.config_file, thread_num=1)
+
+        for _ in range(100):
+            eng.next_step()
+
+        eng.set_replay_file("replay2.txt")
+
+        for _ in range(100):
+            eng.next_step()
+
+        del eng
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
